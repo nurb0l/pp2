eaaa68623c34800ec819a3c210f87090a53cd8ae
nurb0l <157495493+nurb0l@users.noreply.github.com>
2024-02-10 11:50:11 +0600

    while_loops.py

---
+++ lab2/while_loops.py
@@ -0,0 +1,100 @@
+"""
+Python has two primitive loop commands:
+
+while loops
+for loops
+
+"""
+
+"""
+Example
+
+"""
+
+"""
+Print i as long as i is less than 6:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+"""
+
+"""
+Exit the loop when i is 3:
+
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+"""
+
+"""
+Continue to the next iteration if i is 3:
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+"""
+
+"""
+Print a message once the condition is false:
+
+i = 1
+while i < 6:
+     print(i)
+     i += 1
+else:
+    print("i is no longer less than 6")
+
+"""
+
+"""
+    Exercises:
+
+"""
+
+"""
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+"""
+
+"""
+i = 1
+while i < 6:
+    if i == 3:
+        break
+    i += 1
+
+"""
+
+"""
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+"""
+
+"""
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+    
+"""
