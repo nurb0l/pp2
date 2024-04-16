f0b7cfe27073c4d21681d912f92f53cf22b75916
nurb0l <157495493+nurb0l@users.noreply.github.com>
2024-04-16 17:47:07 +0500

    Update paint.py

--- lab 8/paint.py
+++ lab 8/paint.py
@@ -1,50 +1,50 @@
 import pygame
 
-pygame.init() # Initialize pygame
+pygame.init() 
 
 painting = []
 
-timer = pygame.time.Clock() # We need it for future use with fps
+timer = pygame.time.Clock() 
 
-fps = 60 # Set Frames per second
+fps = 60 
 
 activeColor = (0, 0, 0)
 activeShape = 0
 
-w = 1000 # Set Window Size
+w = 1000
 h = 800
 
-screen = pygame.display.set_mode([w, h]) # Set Screen
+screen = pygame.display.set_mode([w, h]) 
 
-pygame.display.set_caption("Paint") # Set Window Title
+pygame.display.set_caption("Paint") 
 
 def drawDisplay():
-    pygame.draw.rect(screen, 'gray', [0, 0, w, 100]) # Draw Display
-    pygame.draw.line(screen, 'black', [0, 100], [w, 100]) # Draw Line separator
+    pygame.draw.rect(screen, 'gray', [0, 0, w, 100]) 
+    pygame.draw.line(screen, 'black', [0, 100], [w, 100])
     rect = [pygame.draw.rect(screen, 'black', [10, 10, 80, 80]), 0]
     pygame.draw.rect(screen, 'white', [20, 20, 60, 60])
     circ = [pygame.draw.rect(screen, 'black', [100, 10, 80, 80]), 1]
     pygame.draw.circle(screen, 'white', [140, 50], 30)
-    blue = [pygame.draw.rect(screen, (0, 0, 255), [w - 35, 10, 25, 25]), (0, 0, 255)] # Draw colors
-    red = [pygame.draw.rect(screen, (255, 0, 0), [w - 35, 35, 25, 25]), (255, 0, 0)] # Draw colors
-    green = [pygame.draw.rect(screen, (0, 255, 0), [w - 60, 10, 25, 25]), (0, 255, 0)] # Draw colors
-    yellow = [pygame.draw.rect(screen, (255, 255, 0), [w - 60, 35, 25, 25]), (255, 255, 0)] # Draw colors
-    black = [pygame.draw.rect(screen, (0, 0, 0), [w - 85, 10, 25, 25]), (0, 0, 0)] # Draw colors
-    purple = [pygame.draw.rect(screen, (255, 0, 255), [w - 85, 35, 25, 25]), (255, 0, 255)] # Draw colors
-    eraser = [pygame.draw.rect(screen, (255, 255, 255), [w - 150, 20, 25, 25]), (255, 255, 255)] # Draw Eraser
+    blue = [pygame.draw.rect(screen, (0, 0, 255), [w - 35, 10, 25, 25]), (0, 0, 255)]
+    red = [pygame.draw.rect(screen, (255, 0, 0), [w - 35, 35, 25, 25]), (255, 0, 0)] 
+    green = [pygame.draw.rect(screen, (0, 255, 0), [w - 60, 10, 25, 25]), (0, 255, 0)] 
+    yellow = [pygame.draw.rect(screen, (255, 255, 0), [w - 60, 35, 25, 25]), (255, 255, 0)] 
+    black = [pygame.draw.rect(screen, (0, 0, 0), [w - 85, 10, 25, 25]), (0, 0, 0)] 
+    purple = [pygame.draw.rect(screen, (255, 0, 255), [w - 85, 35, 25, 25]), (255, 0, 255)] 
+    eraser = [pygame.draw.rect(screen, (255, 255, 255), [w - 150, 20, 25, 25]), (255, 255, 255)] 
     return [blue, red, green, yellow, black, purple, eraser], [rect, circ]
 
 def drawPaint(paints):
     for paint in paints:
         if paint[2] == 1:
-            pygame.draw.circle(screen, paint[0], paint[1], 15) # Draw Paint
+            pygame.draw.circle(screen, paint[0], paint[1], 15) 
         elif paint[2] == 0:
-            pygame.draw.rect(screen, paint[0], [paint[1][0]-15, paint[1][1]-15, 30, 30]) # Draw Paint
+            pygame.draw.rect(screen, paint[0], [paint[1][0]-15, paint[1][1]-15, 30, 30])
 def draw():
     global activeColor, activeShape, mouse
     if mouse[1] > 100:
         if activeShape == 0:
-            pygame.draw.rect(screen, activeColor, [mouse[0]-15, mouse[1]-15, 30, 30]) # Draw
+            pygame.draw.rect(screen, activeColor, [mouse[0]-15, mouse[1]-15, 30, 30]) 
         if activeShape == 1:
             pygame.draw.circle(screen, activeColor, mouse, 15)
 
